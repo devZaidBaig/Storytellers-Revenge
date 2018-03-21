8052a8460112b22d55544a6a4b57490410add2dc
A. Zaid Baig <zaidgogreen@gmail.com>
2018-03-21 22:44:44 +0530

    initial commit 1.1

---
+++ Assets/Scripts/EndCreditsAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndCreditsAudio : MonoBehaviour {
+
+    public GameObject Sphere;
+    AudioSource endCredit;
+    VideoController playing;
+
+    private void Start()
+    {
+        playing = Sphere.GetComponent<VideoController>();
+        endCredit = gameObject.GetComponent<AudioSource>();
+    }
+
+    void Update () {
+        if (!playing.audioSource.isPlaying && playing.finished)
+        {
+            endCredit.Play();
+        }
+	}
+}
---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoController : MonoBehaviour {
+
+    private UnityEngine.Video.VideoPlayer videoPlayer;
+
+    public GameObject Button;
+
+ 
+    public AudioSource audioSource;
+
+    private AudioSource videoAudio;
+
+    public bool finished = false;
+
+
+    void Start()
+    {
+        videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer>();
+        videoAudio = GetComponent<AudioSource>();
+
+        if (videoPlayer.clip != null)
+        {
+            videoPlayer.EnableAudioTrack(0, true);
+            videoPlayer.SetTargetAudioSource(0, audioSource);
+        }
+    }
+
+    //Check if input keys have been pressed and call methods accordingly.
+    public void PlayVideo()
+    {
+        audioSource.Play();
+        if (videoPlayer.isPrepared)
+        {
+            videoPlayer.Play();
+            videoAudio.Play();
+            finished = true;
+            //audioSource.Play();
+        }
+        else
+        {
+            Debug.Log("The video is still loading...");
+        }
+    }
+
+    public void PauseVideo()
+    {
+        videoPlayer.Pause();
+        videoAudio.Pause();
+    }
+
+    public void Restart()
+    {
+        videoAudio.Play();
+        videoPlayer.frame = 0;
+        audioSource.Play();
+        Button.SetActive(false);
+        finished = false;
+    }
+
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    private void Update()
+    {
+        if (!audioSource.isPlaying && finished)
+        {
+            Button.SetActive(true);
+        }
+
+        if (videoPlayer.isPrepared)
+        {
+            Debug.Log("Now you can play the video!");
+        }
+    }
+}

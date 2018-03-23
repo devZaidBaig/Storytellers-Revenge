01bc3cde15b6d1305cefe6f55d40529a3b3fcb5b
A. Zaid Baig <zaidgogreen@gmail.com>
2018-03-23 21:00:40 +0530

    commit 1.4
    Reduced 3 scenes to one main scene

--- Assets/Scripts/VideoController.cs
+++ Assets/Scripts/VideoController.cs
@@ -2,26 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class VideoController : MonoBehaviour {
 
     private UnityEngine.Video.VideoPlayer videoPlayer;
 
     public GameObject Button;
+    public AudioSource taj;
+    public AudioSource germany;
+    public AudioSource iceland;
 
- 
+    public GvrAudioSource GvrTaj;
+    public GvrAudioSource GvrGermany;
+    public GvrAudioSource GvrIceland;
+
     public GvrAudioSource audioSource;
 
+    public GameObject EndCredit;
+    public GameObject buttonEnd;
+
+    public ParticleSystem rain;
     private AudioSource videoAudio;
     bool isPlaying = true;
-
     public bool finished = false;
 
 
     void Start()
     {
+        videoAudio = taj;
+        audioSource = GvrTaj;
         videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer>();
-        videoAudio = GetComponent<AudioSource>();
         audioSource.enabled = true;
         if (videoPlayer.clip != null)
         {
@@ -36,7 +47,9 @@ public class VideoController : MonoBehaviour {
         if (videoPlayer.isPrepared)
         {
             if (isPlaying)
-            { audioSource.Play(); }
+            { audioSource.Play();
+                finished = true;
+            }
             videoPlayer.Play();
             videoAudio.Play();
             finished = true;
@@ -49,6 +62,7 @@ public class VideoController : MonoBehaviour {
 
     public void PauseVideo()
     {
+        finished = false;
         videoPlayer.Pause();
         if (audioSource.isPlaying) { audioSource.Pause(); isPlaying = true; }
         else { isPlaying = false; }
@@ -57,17 +71,42 @@ public class VideoController : MonoBehaviour {
 
     public void Restart()
     {
+        videoAudio.Stop();
         videoAudio.Play();
         videoPlayer.frame = 0;
+        audioSource.Stop();
         audioSource.Play();
         Button.SetActive(false);
-        finished = false;
+        finished = true;
         isPlaying = true;
     }
 
     public void ChangeScene(string scene)
     {
-        SceneManager.LoadScene(scene);
+        videoAudio.Stop();
+        videoPlayer.Stop();
+        if (scene == "iceland")
+        {
+            videoPlayer.url = "https://www.dropbox.com/s/850499ckn7xgbjj/Iceland.mp4?dl=1";
+            videoAudio = iceland;
+            audioSource = GvrIceland;
+            rain.Play();
+        }
+        else if(scene == "germany")
+        {
+            videoPlayer.url = "https://www.dropbox.com/s/13d5tggpzqklmsb/travelvideo.mp4?dl=1";
+            videoAudio = germany;
+            audioSource = GvrGermany;
+        }
+        if (videoPlayer.isPlaying)
+        {
+            videoAudio.Play();
+            audioSource.Play();
+        }
+
+        EndCredit.SetActive(true);
+        Button.SetActive(false);
+        Button = buttonEnd;
     }
 
     private void Update()
